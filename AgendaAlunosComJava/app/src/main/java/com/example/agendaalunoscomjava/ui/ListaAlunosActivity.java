93eb86e5de586de3e750a35a88c33d3c9db7a92a
Thailan <thailan.godoy@agiltec.com>
2022-02-05 19:54:47 -0300

    logic finished

--- AgendaAlunosComJava/app/src/main/java/com/example/agendaalunoscomjava/ui/ListaAlunosActivity.java
+++ AgendaAlunosComJava/app/src/main/java/com/example/agendaalunoscomjava/ui/ListaAlunosActivity.java
@@ -2,6 +2,7 @@ package com.example.agendaalunoscomjava.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -13,6 +14,8 @@ import com.example.agendaalunoscomjava.R;
 import com.example.agendaalunoscomjava.dao.AlunoDao;
 import com.example.agendaalunoscomjava.model.Aluno;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
 
 
 public class ListaAlunosActivity extends AppCompatActivity {
@@ -52,11 +55,13 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
     private void configLista() {
         ListView listaDeAlunos = findViewById(R.id.activitymainlistadealunos);
+        List<Aluno> alunos = dao.todos();
         listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_expandable_list_item_1, dao.todos()));
         listaDeAlunos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-
+                Aluno alunoEscolhido = alunos.get(position);
+                Log.i("posicao do aluno", "" + alunoEscolhido);
             }
         });
     }
